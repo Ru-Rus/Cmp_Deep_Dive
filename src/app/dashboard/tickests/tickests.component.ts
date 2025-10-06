3db1eff88c8b79ad5906b227094d83576ba1b45f
Ru-Rus <marquilleroruselljohn@gmail.com>
2025-10-06 09:50:46 +0800

    property binding repetition

---
+++ src/app/dashboard/tickests/tickests.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tickests',
+  standalone: true,
+  imports: [],
+  templateUrl: './tickests.component.html',
+  styleUrl: './tickests.component.css'
+})
+export class TickestsComponent {
+
+}
