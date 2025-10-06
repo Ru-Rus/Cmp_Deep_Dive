a98c0b7afeb89da2d102f2d67191246a4885e0d7
Ru-Rus <marquilleroruselljohn@gmail.com>
2025-10-06 11:57:33 +0800

    Using host elements like regular elements in SCSS

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,11 +1,12 @@
-import { Component, input } from '@angular/core';
+import { Component, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
   standalone: true,
   imports: [],
   templateUrl: './control.component.html',
-  styleUrls: ['./control.component.css', './control.component.scss' ]
+  styleUrls: ['./control.component.css', './control.component.scss' ],
+  encapsulation: ViewEncapsulation.None
 })
 export class ControlComponent {
   label = input.required <string>();
