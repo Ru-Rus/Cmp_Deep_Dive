48e7611eb12363e71d2ea2b4669de3d8cdf7823c
Ru-Rus <marquilleroruselljohn@gmail.com>
2025-10-06 15:51:25 +0800

    working with template variables

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from "../../../shared/button/button.component";
 import { ControlComponent } from "../../../shared/control/control.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ButtonComponent, ControlComponent],
+  imports: [ButtonComponent, ControlComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css',
   host:{
@@ -13,5 +14,8 @@ import { ControlComponent } from "../../../shared/control/control.component";
   }
 })
 export class NewTicketComponent {
-
+  onSubmit(titleElement: HTMLInputElement){
+    const inputTitle = titleElement.value;
+    console.log(inputTitle);
+  }
 }
